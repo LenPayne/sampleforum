a33a0afbc39967d133f06a7099d331ca21b5b582
Len Payne <len.payne@lambtoncollege.ca>
2015-03-09 10:11:12 -0400

    Added POST

--- src/main/java/service/Forum.java
+++ src/main/java/service/Forum.java
@@ -8,7 +8,10 @@ package service;
 
 import database.Database;
 import java.sql.*;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
@@ -23,5 +26,17 @@ public class Forum {
     @Produces("application/json")
     public Response getAll() {
         return Response.ok(Database.getResults("SELECT * FROM forum")).build();
+    }
+    
+    @POST
+    @Consumes("application/json")
+    public Response addOne(JsonObject json) {
+        String name = json.getString("name");
+        String text = json.getString("text");
+        int result = Database.doUpdate("INSERT INTO forum (name, postText, time) VALUES (?, ?, NOW())", name, text);
+        if (result <= 0)
+            return Response.status(500).build();
+        else
+            return Response.ok().build();
     }
 }
